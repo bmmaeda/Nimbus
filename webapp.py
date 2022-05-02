5bd574de627650692d16697e9e342e0d94cc465c
Bruno <bruno_maeda00@hotmail.com>
2022-05-02 08:39:22 -0300

    0.1.1

--- webapp.py
+++ webapp.py
@@ -1,20 +1,30 @@
 __version__ = '0.1.0'
 import streamlit as st
 import yfinance as yf
-from datetime import date
 import pandas as pd
+from PIL import Image
+from datetime import date
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly, plot_components_plotly
 from plotly import graph_objs as go
+
+#add logo dastratus
+col1, col2 = st.columns([3,1])
+with col1:
+    st.title('Stratus')
+with col2:
+    image = Image.open('logo_blue.png')
+    st.image(image, width=150)
 
 #define a data de início e fim
 data_inicio = '2017-01-01'
 data_fim = date.today().strftime('%Y-%m-%d')
 
 #cabeçalho
-st.title('Análise de Ações')
-st.sidebar.header('Escolha a ação')
-n_dias = st.slider('Quantidade de dias de previsão' , 30,365)
+
+st.subheader('Análise de Ações')
+st.sidebar.subheader('Escolha uma ação:')
+n_dias = st.sidebar.select_slider('Quantidade de dias de previsão',options=[30, 60, 120, 240, 360])
 
 #coleta dos dados no .csv
 def pegar_dados_acoes():
@@ -43,11 +53,9 @@ st.write(df_valores.tail(10))
 #plota o gráfico de preços das açoes
 st.subheader('Gráfico de Preços')
 fig = go.Figure(data=[go.Candlestick(x=df_valores['Date'],open=df_valores['Open'],high=df_valores['High'],low=df_valores['Low'],close=df_valores['Close'])])
-#fig.add_trace(go.Candlestick(x=df_valores['Date'],y=df_valores['Close'],name='Preço de Fechamento',line_color='blue'))
-#fig.add_trace(go.Candlestick(x=df_valores['Date'],y=df_valores['Open'],name='Preço de Abertura',line_color='red'))
 st.plotly_chart(fig)
 
-#define as variáveis de treino
+''''#define as variáveis de treino
 df_treino = df_valores[['Date','Close']]
 df_treino = df_treino.rename(columns={'Date':'ds', 'Close': 'y'})
 
@@ -60,12 +68,8 @@ previsao = modelo.predict(futuro)
 #plota os gráficos do treino
 st.subheader('Previsão')
 st.write(previsao[['ds', 'yhat','yhat_lower','yhat_upper']].tail(n_dias))
-st.subheader('Gráfico de Preços')
 
 #gráfico 1
+st.subheader('Gráfico de Preços')
 grafico1 = plot_plotly(modelo, previsao)
-st.plotly_chart(grafico1)
-
-#gráfico2
-grafico2 = plot_components_plotly(modelo, previsao)
-st.plotly_chart(grafico2)
+st.plotly_chart(grafico1)'''
